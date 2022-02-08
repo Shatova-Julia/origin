c00de7166e970bb54473ab3203ef7752b4f43810
Aleksandr_z <30384395+NickManibus@users.noreply.github.com>
2022-02-08 13:05:25 +0300

    Zemiantsev_Lesson_6
    Lesson_6

--- Lesson_06/hw_01.py
+++ Lesson_06/hw_01.py
@@ -2,3 +2,20 @@
 # Затем результат преобразовать в байтовый вид в кодировке ‘Latin1’
 # и результат снова декодировать в строку
 # (результаты всех преобразований выводить на экран).
+
+
+
+bytes_decode = b'r\xc3\xa9sum\xc3\xa9'
+print(bytes_decode)
+
+decode = bytes_decode.decode()
+print(decode)
+
+encode_Latin = decode.encode('Latin1')
+print(encode_Latin)
+
+bytes_decode2 = b'r\xe9sum\xe9'
+decode_two = bytes_decode2.decode('Latin1')
+print(decode_two)
+
+
--- Lesson_06/hw_04.py
+++ Lesson_06/hw_04.py
@@ -1,3 +1,30 @@
 # Прочитать сохранённый json-файл и записать данные на диск в csv-файл,
 # первой строкой которого озаглавив каждый столбец
 # и добавив новый столбец “телефон”.
+
+import csv
+
+b_dict = {
+          '159612': ('Tom', 23, 5554378),
+          '310640': ('Noah', 32, 5558732),
+          '044208': ('Jacob', 41, 5559065),
+          '233648': ('Liam', 21, 5552314),
+          '053872': ('Michael', 27, 5550956),
+          '403600': ('Anderson', 54, 5552343)
+}
+
+with open('name_two.json', 'r') as f:
+    with open('name_three.csv', 'w', encoding='UTF-8') as w_file:
+     data_file = csv.writer(w_file, delimiter='│', lineterminator="\r")
+     data_file.writerow(["id_Name", "Name", "Age", "Telefon"])
+     data_file.writerow(["159612", "Tom", "23", "5554378"])
+     data_file.writerow(["310640", "Noah", "32", "5558732"])
+     data_file.writerow(["044208", "Jacob", "41", "5559065"])
+     data_file.writerow(["233648", "Liam", "21", "5552314"])
+     data_file.writerow(["053872", "Michael", "27", "5550956"])
+     data_file.writerow(["403600", "Anderson", " 54", "5552343"])
+
+
+with open('name_three.csv', 'r', encoding='UTF-8') as r_file:
+    file_reader = csv.reader(r_file, delimiter='│')
+    count = 0
